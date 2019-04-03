c55a26071a4b7aef957eb0f90a9421b3530d582e
Pinet Nicolas <npinet@neocasesoftware.com>
2019-04-03 07:53:47 +0200

    test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Librairie;
+using Librairie.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CatalogJsonImportEmpty()
+        {
+            IStore store = new Store();
+            store.Import("");
+            Assert.AreEqual<int>(store.Quantity("test"), 0);
+        }
+    }
+}
